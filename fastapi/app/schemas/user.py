dbb54051586509a32ce4b692369f4a5b3cc1bcdc
Savan Charola <charolasavan@gmail.com>
2025-08-29 08:24:55 +0530

    remove login request

--- fastapi/app/schemas/user.py
+++ fastapi/app/schemas/user.py
@@ -1,6 +1,17 @@
 from pydantic import BaseModel
+from datetime import date
+from typing import Optional
 
 class UserBase(BaseModel):
     user_name: str
-    user_email: str
     user_password: str
+    email_id: str
+    phone_number: int           
+    dob: date                   
+    gender: str
+    address: str
+    city: str
+    state: str
+    zip_code: int
+    country: str
+    profile_img: Optional[str] = None 
