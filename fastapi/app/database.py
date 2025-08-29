fd08ac380d02dc4dc3a7baf3e170da78ec8a6841
Savan Charola <charolasavan@gmail.com>
2025-08-29 09:13:48 +0530

    add env database url

--- fastapi/app/database.py
+++ fastapi/app/database.py
@@ -11,21 +11,13 @@
 # Base = declarative_base()
 
 
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import os
 
-# Recommended: use environment variable for security
-# Example: mysql+pymysql://user:password@host:port/dbname
 DB_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:root@localhost:3306/ProductApplication")
 
-# Create SQLAlchemy engine
 engine = create_engine(DB_URL)
-
-# Create a configured "Session" class
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base class for ORM models
 Base = declarative_base()
